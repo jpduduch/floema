b083e75d43b0d76bd8f4bacd47cde015cf1f831d
João Paulo Duduch <jpduduch@gmail.com>
2021-07-31 18:45:58 -0300

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path') /* busca o path da aplicação e atribui aos comandos path.etc */
-const Webpack = require('webpack') 
+const Webpack = require('webpack')
 
 /* Webpack é um compilador que consegue juntar todos os arquivos src como .sass e .js e gerar os outputs a serem lidos pelo browser */
 
@@ -21,102 +21,102 @@ const dirNode = 'node_modules'
 // module exports é o comando que exporta as funções etc geradas aqui para serem usadas por outros arquivos
 
 module.exports = {
-	entry: [
-		path.join(dirApp, 'index.js'),
-		path.join(dirStyles, 'index.scss')
-	],
+  entry: [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
 
-	resolve: { 
-		modules: [
-			dirApp,
-			dirShared,
-			dirStyles,
-			dirNode
-		]
-	},
+  resolve: {
+    modules: [
+      dirApp,
+      dirShared,
+      dirStyles,
+      dirNode
+    ]
+  },
 
-	plugins: [
-		new Webpack.DefinePlugin({
-			IS_DEVELOPMENT
-		}),
+  plugins: [
+    new Webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
 
-		// new Webpack.ProvidePlugin({
-		// 	// uma forma de importar libraries para seu projeto. só incluir aqui dentro que já era. o lado ruim é que pode começar a deixar seu projeto pesado se vc perder o controle
-		// }),
+    // new Webpack.ProvidePlugin({
+    // 	// uma forma de importar libraries para seu projeto. só incluir aqui dentro que já era. o lado ruim é que pode começar a deixar seu projeto pesado se vc perder o controle
+    // }),
 
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './shared',
-					to: ''
-				}
-			]
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
 
-		}),
+    }),
 
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css'
-		}),
-		new ImageMinimizerPlugin({
-			minimizerOptions: {
-				// Lossless optimization with custom option
-				// Feel free to experiment with options for better result for you
-				plugins: [
-					["gifsicle", { interlaced: true }],
-					["jpegtran", { progressive: true }],
-					["optipng", { optimizationLevel: 5 }]
-				],
-			},
-		}),
-	],
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }]
+        ]
+      }
+    })
+  ],
 
-	module: {
-		rules: [
-			{ //js
-				test: /\.js$/,
-				use: { loader: 'babel-loader' }
-			},
+  module: {
+    rules: [
+      { // js
+        test: /\.js$/,
+        use: { loader: 'babel-loader' }
+      },
 
-			{ //css
-				test: /\.scss$/,
-				use: [
-					{ loader: MiniCssExtractPlugin.loader, options: { publicPath: '' }   },
-					{ loader: 'css-loader' },
-					{ loader: 'postcss-loader' },
-					{ loader: 'sass-loader' }
-				]
-			},
+      { // css
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: { publicPath: '' } },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
 
-			{ //imagens
-				test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-				loader: 'file-loader',
-				options: {
-					// outputPath: 'images', //coloca numa subpasta
-					name (file){
-						return '[hash].[ext]'
-					}
-				} 
-			},
+      { // imagens
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          // outputPath: 'images', //coloca numa subpasta
+          name (file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
 
-			{ //minifier
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				use: [
-					{
-						loader: ImageMinimizerPlugin.loader
-					},
-				],
-			},
-			{ //webGL
-				test: /\.(glsl|frag|vert)$/,
-				loader: 'glslify-loader',
-				exclude: /node_modules/
-			},
-			{ //webGL
-				test: /\.(glsl|frag|vert)$/,
-				loader: 'raw-loader',
-				exclude: /node_modules/
-			}
-		]
-	}
+      { // minifier
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader
+          }
+        ]
+      },
+      { // webGL
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      },
+      { // webGL
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
