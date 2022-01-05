30285215ec1f3d1202af42610ff10dec6633225e
João Duduch <joao.duduch@PDX-FVFG810DQ05N.local>
2022-01-05 23:40:44 +0100

    Setting up Prismic pt 3

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const path = require('path');
-const express = require('express');
-const app = express();
-const port = 8004;
+const path = require('path')
+const express = require('express')
+const errorHandlers = require('errorhandler')
+const app = express()
+const port = 8004
 
-const Prismic = require('@prismicio/client');
-var PrismicDOM = require('prismic-dom');
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
 // const { get } = require('express/lib/response');
 // no need of a Prismic variable to API endpoint, since we're using dotenv for this. Ref: Luis Bizarro video 'Integrating Prismic with your project: 1h aprox'
 
@@ -19,13 +20,14 @@ const initApi = req => {
   })
 }
 
-
 // step 5: adding a link resolver. Ref: Luis Bizarro video 'Integrating Prismic with your project: 1h05m39s
 
 const handleLinkResolver = doc => {
   // no code for now since we're doing this manually
   return '/'
 }
+
+app.use(errorHandlers())
 
 // step 6: create a middleware to add the prismic context.  Ref: Luis Bizarro video 'Integrating Prismic with your project: 1h07m00s
 
@@ -36,51 +38,55 @@ app.use((req, res, next) => {
   }
   // add PrismicDOM in locals to access them in templates.
   res.locals.PrismicDOM = PrismicDOM
+
   next()
 })
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-app.locals.basedir = app.get('views');
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.locals.basedir = app.get('views')
 
 // step 8: add queries to your routes. Ref: Luis Bizarro video 'Integrating Prismic with your project: 1h09m50s (it should be added on each route of express.js below)
 
 app.get('/', async (req, res) => {
-  res.render('pages/home');
-});
+  res.render('pages/home')
+})
 
 app.get('/about', async (req, res) => {
-    initApi(req).then( api => {
-      api.query(
-        Prismic.Predicates.any('document.type', ['meta', 'about'])
-      ).then( response => {
-        const { results } = response
-        const [ about, meta ] = results
-        
-        console.log(about, meta)
+  const api = await initApi(req)
+  const meta = await api.getSingle('meta')
+  const about = await api.getSingle('about')
 
-        // response is the response object. Render your views here
-        // res.render('pages/about', {
-        //   document: response.results[0]
-        // })
-        
-        res.render('pages/about', {
-          about,
-          meta
-        })
+  // response is the response object. Render your views here
+  // res.render('pages/about', {
+  //   document: response.results[0]
+  // })
 
-      })
-    })
+  res.render('pages/about', {
+    about,
+    meta
+  })
 })
 
 app.get('/detail/:uid', async (req, res) => {
-    res.render('pages/detail')
+  const api = await initApi(req)
+  const meta = await api.getSingle('meta')
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  console.log('product ', product)
+
+  res.render('pages/detail', {
+    meta,
+    product
+  })
 })
 
 app.get('/collections', (req, res) => {
-    res.render('pages/collections')
+  res.render('pages/collections')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
