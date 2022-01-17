9829e6111925e1b9f9fc1be0add9d3f918653696
João Duduch <jpduduch@gmail.com>
2022-01-17 09:47:38 +0100

    Setting up sass

--- app/index.js
+++ app/index.js
@@ -1,4 +1,31 @@
-import jubaBhai from 'images/jubabhai.jpeg'
-import yeezy from 'images/20.webp'
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
 
-console.log(jubaBhai, yeezy)
+class App {
+  constructor () {
+    console.log('App')
+    this.createContent()
+    this.createPages()
+  }
+
+  createContent () {
+    this.content = document.querySelector('.content')
+    this.template = this.content.getAttribute('data-template')
+
+    console.log(this.template)
+  }
+
+  createPages () {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+    // console.log(this.pages)
+  }
+}
+
+new App()
